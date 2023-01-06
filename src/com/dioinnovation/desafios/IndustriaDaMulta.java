dbd443bdc47900c948997266c023354a6b9ce6fa
Daniela-ddlm <danieladelmondes191@gmail.com>
2023-01-06 12:22:33 -0300

    Conclusão Desafios Finais

---
+++ src/com/dioinnovation/desafios/IndustriaDaMulta.java
@@ -0,0 +1,36 @@
+/*
+Desafio: A prefeitura de uma cidade A instalou uma câmera para verificar quem estava acima da velocidade permitida na rodovia da entrada da cidade. Porém, ela é uma cidade extremamente movimentada e a prefeitura não deu conta de enviar as multas necessárias para quem ultrapassasse a velocidade permitida de 60km/h.
+O seu papel será o de criar um programa que verifique os dados recebidos da câmera e envie uma mensagem dizendo se o motorista será multado ou não.
+
+Entrada: A entrada vai ser a velocidade do motorista.
+
+Saída: A saída será uma mensagem no console dizendo se o motorista foi multado ou não. (sem as aspas)
+
+Exemplo: Entrada = 60 / Saída = "Não foi multado"; Entrada = 68 / Saída = "Foi multado".
+ */
+
+package com.dioinnovation.desafios;
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.
+
+import java.util.Scanner;
+public class IndustriaDaMulta {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Velocidade: ");
+        int velocidadeAtual = leitor.nextInt();
+        int limite = 60;
+
+        if (velocidadeAtual > limite) {
+            System.out.println("Foi multado");
+        } else {
+            System.out.println("Nao foi multado");
+        }
+
+
+        // TODO: Crie a condição necessária para verificar se o motorista, de acordo com a entrada, foi multado ou não.
+
+    }
+}
---
+++ src/com/dioinnovation/desafios/LojinhaDeDoces.java
@@ -0,0 +1,31 @@
+/*
+Desafio: Uma lojinha de doces tem muitos clientes por ser ao lado de uma escola. Para aumentar as vendas o dono resolveu colocar uma máquina onde os clientes, principalmente as crianças que estão muito acostumadas com a tecnologia, poderiam colocar suas moedas e receber dois doces aleatórios. Cada 1 real irá render 2 doces aleatórios.
+
+Entrada: A entrada será a quantidade de dinheiro que cada cliente, um por vez, possui.
+
+Saída: A saída deverá ser a quantidade de doces que cada cliente conseguiu obter. (sem as aspas)
+
+Exemplo: Entrada = 10 / Saída = "O cliente obteve 20 doces" ; Entrada = 20 / Saída = "O cliente obteve 40 doces".
+ */
+
+package com.dioinnovation.desafios;
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.
+
+import java.util.Scanner;
+public class LojinhaDeDoces {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("moeda ");
+        int moeda = leitor.nextInt();
+        int doce = 2;
+
+        int resultado = moeda * doce;
+        System.out.println("O cliente obteve " + resultado + " doces");
+
+        // TODO: Crie a condição necessária para que cada cliente saiba quantos doces vai obter de acordo com a entrada
+
+    }
+}
