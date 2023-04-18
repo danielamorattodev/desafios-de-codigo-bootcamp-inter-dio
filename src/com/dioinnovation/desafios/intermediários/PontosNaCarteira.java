8bbb1b09e3f002838e3af63576d5d725df51a876
Natália Nonemacher <112293263+Natalianone@users.noreply.github.com>
2023-04-18 16:18:53 +0100

    Update PontosNaCarteira.java
    Hello! I am doing the challenge, and i propose these changes so that the system approves the result. What do you think?

--- src/com/dioinnovation/desafios/intermediários/PontosNaCarteira.java
+++ src/com/dioinnovation/desafios/intermediários/PontosNaCarteira.java
@@ -12,27 +12,25 @@ Exemplo: Entrada = 85/ 1; Saída = "1 multa. Nao levou pontos na carteira" ; Ent
 
 */
 
-
-package com.dioinnovation.desafios.intermediários;
-// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:
-// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
-// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.
-
-import java.util.Scanner;
-
-public class PontosNaCarteira {
+import java.util.Scanner; 
+    
+public class Program {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-
-        System.out.println("Velocidade");
         int velocidade = leitor.nextInt();
         int multas = leitor.nextInt();
+        
+        // TODO: Crie a condição necessária para verificar se os carros estão acima da velocidade
+        boolean limiteVelocidade = velocidade <= 80;
 
-       if (velocidade > 80){
-           multas++;
-       }
-        System.out.println(multas >= 3 ? multas + " multas. Levou pontos na carteira" : multas + " multas. Não levou pontos na carteira");
-
-
+        // TODO2: Crie a condição para verificar se os motoristas receberão ou não pontos na carteira de habilitação
+        if(limiteVelocidade == false){
+          multas++;
+        }
+        if(multas >= 3){
+          System.out.println(multas + " multas. " + "Levou pontos na carteira");
+        }else{
+          System.out.println(multas + " multas. " + "Nao levou pontos na carteira");
+        }
     }
 }
